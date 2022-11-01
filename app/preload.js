5d3c27be9212b774cbf4b3a2213685c57179e5fb
Richard <rtaggart@us.ibm.com>
2022-11-01 08:48:51 -0400

    feat: add electron setup

---
+++ app/preload.js
@@ -0,0 +1,35 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ * 
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ * See also: 2-way communication
+ *   https://www.electronjs.org/docs/latest/tutorial/ipc#pattern-2-renderer-to-main-two-way
+ */
+
+ window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('myAPI', {
+  desktop: true,
+})
+
+contextBridge.exposeInMainWorld('mainAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title),
+  write: (data) => ipcRenderer.send('write', data),
+})
+
+contextBridge.exposeInMainWorld('dataStore', {
+  write: (session_id, data) => ipcRenderer.invoke('datastore:write', session_id, data)
+})
